516035191237c9034ad6365a69edd27ad3044ad9
Raúl Uranga <ruranga@gmail.com>
2012-03-05 12:29:06 -0600

    Initial Commit

---
+++ ShareKitTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ShareKitTest
+//
+//  Created by Raul Uranga on 3/5/12.
+//  Copyright (c) 2012 GrupoW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ShareKitTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ShareKitTest
+//
+//  Created by Raul Uranga on 3/5/12.
+//  Copyright (c) 2012 GrupoW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
